e3a67a6d24fda4198f35650ae395d5fa400300cc
Rikesh Thapa <rikeshthapa@rikeshs-mbp-2.attlocal.net>
2019-12-01 00:32:10 -0600

    Adding database functions, inserts and read using psycopg2

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,24 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+
+DBNAME = "forum"
+
+#POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+    db = psycopg2.connect(database = DBNAME)
+    c = db.cursor()
+    c.execute("SELECT content, time FROM posts order by time desc;")
+    return c.fetchall()
+    """Return all posts from the 'database', most recent first."""
+    db.close()
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+    db = psycopg2.connect(database = DBNAME)
+    c = db.cursor()
+    c.execute("insert into posts values ('%s')" % content)
+    db.commit()
+    db.close()
